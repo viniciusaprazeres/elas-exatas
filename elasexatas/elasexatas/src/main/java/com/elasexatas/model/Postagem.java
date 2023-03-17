82bd6b815c5122896d37d14f16897f7fdd3d2de8
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-03-17 15:04:58 -0300

    Criação da Model

---
+++ elasexatas/elasexatas/src/main/java/com/elasexatas/model/Postagem.java
@@ -0,0 +1,89 @@
+package com.elasexatas.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo titulo é Obrigatório!")
+	@Size(min = 5, max = 255, message = "O atributo título deve conter no mínimo 05 e no máximo 255 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo conteúdo é Obrigatório?")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String conteudo;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@Size(min = 5, max = 255, message = "O atributo link da imagem deve conter 5 e no máximo 255 caracteres")
+	private String imagem;
+	
+	@Size(min = 5, max = 255, message = "O atributo link deve conter 5 e no máximo 255 caracteres")
+	private String link;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+
+}
---
+++ elasexatas/elasexatas/src/test/java/com/elasexatas/ElasexatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasexatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasexatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
