d1023bd91d74d7648b6ece0092a1fa07f5a86f6e
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-22 11:51:49 -0300

    Criação dos métodos CRUD do recurso Tema

---
+++ elasexatas/elasexatas/src/main/java/com/elasexatas/model/Tema.java
@@ -0,0 +1,55 @@
+package com.elasexatas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório.")
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotNull(message = "O atributo descrição é opcional")
+	@Size(min = 10, max = 500, message = "O atributo descrição deve conter no mínimo 10 e no máximo 500 caracteres")
+	private String descricao;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
---
+++ elasexatas/elasexatas/src/main/java/com/elasexatas/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.elasexatas.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.elasexatas.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+}
