ffe6e949ff6a5edf4ac34960709af71ffad0fa90
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-03-23 14:29:55 -0300

    Adicionado as classes controller

---
+++ elasexatas/elasexatas/src/main/java/com/elasexatas/repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.elasexatas.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.elasexatas.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	public List<Postagem>  findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+}
